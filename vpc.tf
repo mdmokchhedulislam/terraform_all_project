f5296cb1ff12c5d56f96f09e056b9c3a135ca8dc
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-07-11 10:54:12 +0600

    full ec2 instance creations successfully

--- vpc.tf
+++ vpc.tf
@@ -33,10 +33,47 @@ resource "aws_route_table" "public_route_table" {
     }
   
 }
-
-
-
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+
+resource "aws_security_group" "ec2_security_group" {
+  name   = "ec2_security_group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "my-key"
+  public_key = file("C:/Users/mokch/.ssh/id_rsa.pub")  # Windows user
+}
+
+
+resource "aws_instance" "ec2_instance" {
+  ami                    = "ami-0c94855ba95c71c99"  # Amazon Linux 2 in us-east-1 (update as needed)
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = aws_key_pair.ec2_key_pair.key_name
+  associate_public_ip_address = true
+
+    tags = {
+    Name = "terraform-ec2"
+  }
+}
+
